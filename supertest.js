473a7ec1ee2bba47189dd237507df8f64b588d53
Hưng Thịnh <33023931+HungThinhIT@users.noreply.github.com>
2022-04-16 23:34:47 +0700

    Update supertest.js

--- supertest.js
+++ supertest.js
@@ -6,6 +6,6 @@ var obj = JSON.parse($response.body);
 obj.vip = 1;
 obj.vip_lifetime: 1,
 obj.vip_plus: 1,
-obj.ip_plus_life_time: 1,
+obj.vip_plus_life_time: 1,
 
 $done({body: JSON.stringify(obj)});
